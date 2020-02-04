72af87154e6f9d6e8c288d73170103af80f0b3c8
Frederik Lau Rasmussen <frederik-lau@live.dk>
2020-02-04 14:10:30 +0100

    Created different classes

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Store
 {
@@ -6,7 +10,7 @@ namespace Store
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
